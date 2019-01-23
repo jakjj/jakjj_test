4c61c634d1853f8fa1e4964aa95d626f213ffbbb
jakjj <1611835174@qq.com>
2019-01-23 18:02:55 +0800

    swagger

--- family/business/src/main/java/org/wz/BusinessApplication.java
+++ family/business/src/main/java/org/wz/BusinessApplication.java
@@ -1,5 +1,6 @@
 package org.wz;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableSwagger2Doc
 public class BusinessApplication {
    /* @Bean
     @LoadBalanced
--- family/config/src/main/java/org/wz/ConfigApplication.java
+++ family/config/src/main/java/org/wz/ConfigApplication.java
@@ -2,7 +2,14 @@ package org.wz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -10,4 +17,5 @@ public class ConfigApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigApplication.class,args);
     }
+
 }

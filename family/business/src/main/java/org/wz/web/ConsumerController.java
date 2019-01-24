17e509e427841cbd2c0be32a6a27b5ba9f5701ee
jakjj <1611835174@qq.com>
2019-01-24 17:45:33 +0800

    集群完成

--- family/business/src/main/java/org/wz/web/ConsumerController.java
+++ family/business/src/main/java/org/wz/web/ConsumerController.java
@@ -1,6 +1,8 @@
 package org.wz.web;
 
 import freemarker.log.Logger;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,11 +14,13 @@ import org.springframework.web.client.RestTemplate;
 import org.wz.service.ConsumerService;
 
 @RestController
+@Api("消费者，add方法")
 public class ConsumerController {
 
     @Autowired
     ConsumerService consumerService;
 
+    @ApiOperation(value="add，实现10+20的计算")
     @RequestMapping(value = "/add",method = RequestMethod.GET)
     public Integer add(){
         return consumerService.add(10,20);
